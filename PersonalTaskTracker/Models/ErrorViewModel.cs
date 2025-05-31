76172f4569aabb9588113990bd8e0836a5d20ea3
sharkioso <tetkin04@mail.ru>
2025-05-31 17:58:00 +0500

    начало положено

---
+++ PersonalTaskTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PersonalTaskTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
